c0a9719d65dc793faae50d56885cb52344cc2904
meghahmath22 <68781857+meghahmath22@users.noreply.github.com>
2022-09-26 18:22:09 +0530

    Update Solution2.cpp

--- Day2/Solution2.cpp
+++ Day2/Solution2.cpp
@@ -3,19 +3,32 @@
 
 using namespace std;
 
-bool twoSum(const vector<int>& nums, int target){
-  unordered_set<int> comps;
-  for (int num: nums){
-    if (comps.find(num) != comps.end()){
-      return true;
-    }
-    comps.insert(target-num);
+vector<int> productExceptSelf(vector<int> & nums){
+  int N = nums.size();
+  vector<int> leftProducts;
+  vector<int> rightProducts;
+
+  leftProducts.push_back(1);
+  rightProducts.push_back(1);
+
+  for (int i=1; i<N; i++){
+    leftProducts.push_back(nums[i-1]*leftProducts[i-1]);
+    rightProducts.push_back(nums[N-i]*rightProducts[i-1]);
   }
-  return false;
+
+  vector<int> prods;
+  for (int i=0;i<N; i++){
+    prods.push_back(leftProducts[i]*rightProducts[N-i-1]);
+  }
+
+ return prods;
 }
 
 int main(){
-  vector<int> nums{2, 7, 5, 11, -7, 6, -131};
-  int target = 1;
-  cout << twoSum(nums, target);
+  vector<int> nums{2, 5, 3, 6};
+  vector<int> res = productExceptSelf(nums);
+  cout << "\nResults\n";
+  for (int r: res){
+    cout << r << endl;
+  }
 }
